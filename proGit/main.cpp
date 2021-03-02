78a6bf4df409d15ffea87d4d34dfba9ba4f68e21
Ishi-so <k019g1077@g.neec.ac.jp>
2021-03-02 16:31:08 +0900

    うんこ追加

--- proGit/main.cpp
+++ proGit/main.cpp
@@ -3,10 +3,22 @@ using namespace std;
 
 int main()
 {
-
+	
 	cout << "Hellow!!GitHub!" << endl;
 
-	cout << "神聖ブランチにて、この行を追加す" << endl;
+	cout << "神聖ブランチン庫" << endl;
+
+
+
+
+
+
+
+
+
+
+	cout <<"うんこ"<< endl;
+
 	system("pause");
 	return 0;
 }
